2caa87a6047ccb2c5e5f2a275ca6910ea8c3509d
katecourt <nkc124@ncl.ac.uk>
2020-11-16 13:01:05 +0000

    add deployment docs

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,16 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ClipsModule } from './clips/clips.module';
 import { LocsModule } from './locs/locs.module';
-
-
-
-
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/screening'),
+    MongooseModule.forRoot(process.env.DB_CONNECTION_STRING, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    }),
     ClipsModule,
     LocsModule,
   ],
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,41 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+import * as bodyParser from 'body-parser';
+dotenv.config();
+
+// checks required env variables are set
+async function loadEnvVars() {
+  const result = await dotenv.config();
+
+  if (result.error) {
+    throw result.error;
+  }
+
+  if (process.env.DB_CONNECTION_STRING === undefined) {
+    console.log('db_connection_string not set');
+  }
+
+  else {
+    console.log('db_connection_string set');
+  }
+}
+
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(3000);
+  try {
+    const app = await NestFactory.create(AppModule);
+    app.use(bodyParser.json({limit: '50mb'}));
+    app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+    app.enableCors();
+    await app.listen(3000);
+  } catch (err) {
+    console.log(err);
+    console.log(process.env.DB_CONNECTION_STRING);
+  }
 }
-bootstrap();
+
+loadEnvVars().then(() => {
+  console.log('load envs');
+  bootstrap();
+});
